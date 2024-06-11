0921e2cd2346f59f36c35adbbc8a356c75b03153
SunCoya <3342824499@qq.com>
2024-06-11 10:52:52 +0800

    不同路径降维

--- 数据结构与算法/src/demo23_DP/Solution1_62.java
+++ 数据结构与算法/src/demo23_DP/Solution1_62.java
@@ -12,4 +12,16 @@ public class Solution1_62 {
         for (int i = 1; i < m; i++) for (int j = 1; j < n; j++) dp[i][j]=dp[i-1][j]+dp[i][j-1];
         return dp[m-1][n-1];
     }
+    //优化，你自己是一行一行整的，那么最后你的dp也可以变成一维的
+    public int uniquePaths2(int m, int n) {
+        int[] dp = new int[n];
+        Arrays.fill(dp,1);
+        for (int j = 1; j < m; j++) {
+            for (int i = 1; i < n; i++) {
+                dp[i]=dp[i]+dp[i-1];
+            }
+        }
+        System.out.println(Arrays.toString(dp));
+        return dp[n-1];
+    }
 }
